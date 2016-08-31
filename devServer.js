016c57e13bb6994e22ccd618058e68b11e6354ab
deng3h <3hdeng@gmail.com>
2016-08-31 12:24:56 +0000

    cloud9, host 0.0.0.0, port 8080

--- devServer.js
+++ devServer.js
@@ -18,11 +18,11 @@ app.get('/', function(req, res) {
 });
 app.use(express.static('assets'));
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(8080, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:8080');
 });
---
+++ myreadme.txt
@@ -0,0 +1,11 @@
+
+devServer.js --> 
+localhost to 0.0.0.0
+3010 to 8080
+
+[error2016.08.31]
+index.js:26 Uncaught Error: 
+imports[1] for react-transform-catch-errors does not look like a React component.
+
+--> 
+$ npm install redbox-react@1.2.6 --save-dev
